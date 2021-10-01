325c3f1556e098323e815274c26e6f996c5964d7
brian <brianhkim58@gmail.com>
2021-09-30 21:43:06 -0700

    Changes by petej

--- src/pages/StarterPage.vue
+++ src/pages/StarterPage.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <RouletteSpinner :length="38" :roll_time="15"></RouletteSpinner>
     <Toolbar1></Toolbar1>
     <Footer2></Footer2>
 
@@ -9,10 +10,18 @@
 <script>
 import Toolbar1 from '../components/ui/landing/toolbar/Toolbar1'
 import Footer2 from '../components/ui/landing/footer/Footer2'
+import RouletteSpinner from '../components/games/RouletteSpinner'
+import { menu2 as menu } from '../components/ui/landing/toolbar/menus'
 export default {
+  data() {
+    return {
+      start_time: Date.now() + (15 * 1000)
+    }
+  },
   components: {
     Toolbar1,
-    Footer2
+    Footer2,
+    RouletteSpinner
 
   }
 }
